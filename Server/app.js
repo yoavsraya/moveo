142c0eccbdad0d372aac8c72e8e57f3946ce5a45
yoavsraya <118468638+yoavsraya@users.noreply.github.com>
2024-10-06 06:00:45 +0300

    try again

--- Server/app.js
+++ Server/app.js
@@ -31,6 +31,7 @@ app.use(cors({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../Front/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
